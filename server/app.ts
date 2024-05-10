77fb241c5bca8e5e3616187a9ac63e954f9012b7
James Olsen <olsenjames1116@gmail.com>
2024-05-10 18:54:18 -0500

    Set up client and server with testing

--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,11 @@
 require('dotenv').config();
 import express, { Express, Request, Response } from 'express';
-const { Pool } = require('pg');
 
-const app: Express = express();
+export const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const pool = new Pool({
-	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DB,
-	host: process.env.POSTGRES_HOST,
-	port: process.env.POSTGRES_PORT,
-});
-
-app.get('/', (req: Request, res: Response) => {
-	pool.query('SELECT * FROM "user"', (error: Error, results: any) => {
-		if (error) throw error;
-		res.json(results.rows);
-	});
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
---
+++ server/utils/db.ts
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+});
+
+module.exports = pool;
