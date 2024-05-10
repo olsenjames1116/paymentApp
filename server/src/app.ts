77f022f2d81772bd6507223897133253eaf73503
James Olsen <olsenjames1116@gmail.com>
2024-05-10 16:01:10 -0500

    Initialize TypeScript in server

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+require('dotenv').config();
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Express + Typescript server');
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
