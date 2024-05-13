50f7cd42b02a5a99f1d094ac908c121f3b3f741c
James Olsen <olsenjames1116@gmail.com>
2024-05-12 21:20:38 -0400

    Pass click handler for account to button component

--- client/src/components/SignUpForm/SignUpForm.tsx
+++ client/src/components/SignUpForm/SignUpForm.tsx
@@ -4,12 +4,19 @@ import PasswordInput from '../PasswordInput/PasswordInput';
 import UsernameInput from '../UsernameInput/UsernameInput';
 
 function SignUpForm() {
+	const validateInput = (
+		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		event.preventDefault();
+		console.log('sign up');
+	};
+
 	return (
 		<form data-testid="sign-up-form" noValidate>
 			<UsernameInput />
 			<PasswordInput />
 			<ConfirmPasswordInput />
-			<FormSubmitButton text="Sign Up" />
+			<FormSubmitButton text="Sign Up" handleClick={validateInput} />
 		</form>
 	);
 }
