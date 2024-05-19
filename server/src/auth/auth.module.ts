be5315c3a1baf79ce688ed3c1988d7ee0d43cda3
James Olsen <olsenjames1116@gmail.com>
2024-05-19 18:50:07 -0400

    Fix errors in auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
