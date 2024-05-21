b242fd2f7cb4e5e38ff15a1bbbbacf3269c298a2
James Olsen <olsenjames1116@gmail.com>
2024-05-21 19:12:36 -0400

    Create a home page component

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import LogInPage from './components/LogInPage/LogInPage';
 import SignUpPage from './components/SignUpPage/SignUpPage';
+import HomePage from './components/HomePage/HomePage';
 
 function Router() {
 	return (
 		<Routes>
+			<Route path="/" element={<HomePage />} />
 			<Route path="/log-in" element={<LogInPage />} />
 			<Route path="/sign-up" element={<SignUpPage />} />
 		</Routes>
---
+++ client/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+	return <div data-testid="home-page">HomePage</div>;
+}
+
+export default HomePage;
