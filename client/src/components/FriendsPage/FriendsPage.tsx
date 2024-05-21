98b9bd96491ea2b26e5d76bed55a815e4ec13135
James Olsen <olsenjames1116@gmail.com>
2024-05-21 19:26:29 -0400

    Route to log-in page if user is unauthenticated

--- client/src/components/FriendsPage/FriendsPage.tsx
+++ client/src/components/FriendsPage/FriendsPage.tsx
@@ -1,4 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 function FriendsPage() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (sessionStorage.length < 1) {
+			navigate('/log-in');
+		}
+	}, []);
+
 	return <div>FriendsPage</div>;
 }
 
--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (sessionStorage.length < 1) {
+			navigate('/log-in');
+		}
+	}, []);
+
 	return (
 		<div data-testid="home-page">
 			<Sidebar />
