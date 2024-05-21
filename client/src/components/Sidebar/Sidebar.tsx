96ca4249c520c71d6d966fb7b119d0fbae111481
James Olsen <olsenjames1116@gmail.com>
2024-05-21 19:21:12 -0400

    Put some placeholder data in Sidebar

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
 function Sidebar() {
-	return <section data-testid="sidebar">Sidebar</section>;
+	return (
+		<section data-testid="sidebar">
+			<h1>SpotMe</h1>
+			<img src="" alt="" />
+			<span>Username</span>
+			<span>Balance</span>
+			<form>
+				<button>Pay</button>
+			</form>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/friends">Friends</Link>
+					</li>
+				</ul>
+			</nav>
+		</section>
+	);
 }
 
 export default Sidebar;
