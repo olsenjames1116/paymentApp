9d66c8452ce7ea48ba4a9d8c23047bda00c73205
James Olsen <olsenjames1116@gmail.com>
2024-05-23 19:53:02 -0400

    Unit test for edit page

--- client/src/components/EditPage/EditPage.tsx
+++ client/src/components/EditPage/EditPage.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function EditPage() {
-	return <div>EditPage</div>;
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (sessionStorage.length < 1) {
+			navigate('/log-in');
+		}
+	});
+
+	return <div data-testid="edit-page">EditPage</div>;
 }
 
 export default EditPage;
