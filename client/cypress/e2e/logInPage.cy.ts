d264f8a2b373d68c5e065daf7fc644debb9b36c9
James Olsen <olsenjames1116@gmail.com>
2024-05-22 20:21:10 -0400

    Write e2e tests for log in page

--- client/cypress/e2e/logInPage.cy.ts
+++ client/cypress/e2e/logInPage.cy.ts
@@ -17,7 +17,37 @@ describe('LogInPage', () => {
 			.find('li')
 			.should('have.text', 'Password must not be empty.');
 	});
-	// should display an error message if the username does not exist in the database.
-	// should display an error if the password does not match the one stored in the database.
-	// should navigate the user to the home page if they are logged in.
+
+	it('should display an error message if the username does not exist in the database.', () => {
+		cy.visit('http://localhost:5173/log-in');
+
+		cy.get('[data-testid="username-input"]').type('demo1');
+		cy.get('[data-testid="password-input"]').type('123');
+		cy.get('[data-testid="form-submit-button"]').click();
+
+		cy.get('[data-testid="username-input-invalid-feedback"]')
+			.find('li')
+			.should('have.text', "User 'demo1' does not exist.");
+	});
+
+	it('should display an error if the password does not match the one stored in the database.', () => {
+		cy.visit('http://localhost:5173/log-in');
+
+		cy.get('[data-testid="username-input"]').type('demo');
+		cy.get('[data-testid="password-input"]').type('456');
+		cy.get('[data-testid="form-submit-button"]').click();
+
+		cy.get('[data-testid="password-input-invalid-feedback"]')
+			.find('li')
+			.should('have.text', 'Password is incorrect.');
+	});
+
+	it('should navigate the user to the home page if they are logged in.', () => {
+		cy.login();
+
+		cy.wait(500);
+		cy.visit('http://localhost:5173/log-in');
+
+		cy.url().should('match', /\/$/);
+	});
 });
