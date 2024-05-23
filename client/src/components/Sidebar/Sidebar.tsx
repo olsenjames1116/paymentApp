3171f5066fd17e3ea537bb8da8b5a8a3e5de7b66
James Olsen <olsenjames1116@gmail.com>
2024-05-22 21:36:53 -0400

    Logic to retrieve user data on mount in sidebar

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -3,9 +3,27 @@ import LogOut from '../LogOut/LogOut';
 import NavItem from '../NavItem/NavItem';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../redux/store';
+import api from '../../axiosConfig';
+import { useEffect } from 'react';
 
 function Sidebar() {
 	const location = useSelector((state: IRootState) => state.location.value);
+
+	useEffect(() => {
+		const getUserInfo = async () => {
+			try {
+				const userInfo = await api.get('/user', {
+					headers: { Authorization: sessionStorage.getItem('access_token') },
+				});
+
+				console.log(userInfo);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getUserInfo();
+	}, []);
 
 	return (
 		<section data-testid="sidebar">
