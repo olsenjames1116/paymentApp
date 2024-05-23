591cf96ef0425b13db2a5e0dfd3d3c507bc1b877
James Olsen <olsenjames1116@gmail.com>
2024-05-22 21:22:45 -0400

    Store pathname on page renders

--- client/src/components/FriendsPage/FriendsPage.tsx
+++ client/src/components/FriendsPage/FriendsPage.tsx
@@ -1,12 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { storeLocation } from '../../redux/state/locationSlice';
 
 function FriendsPage() {
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		if (sessionStorage.length < 1) {
 			navigate('/log-in');
+		} else {
+			document.title = 'Friends';
+			dispatch(storeLocation(window.location.pathname));
 		}
 	}, []);
 
